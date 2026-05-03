3e6ba9d69e5d61d7275dbed46cb5b3e20f5e7bad
quickybrains <quickybrains@gmail.com>
2026-05-03 18:56:03 -0100

    closing file opened for compressing

--- internal/compressor.go
+++ internal/compressor.go
@@ -49,11 +49,6 @@ func (c *Compressor) Compress() ([]byte, error) {
 	w := zip.NewWriter(c.buff)
 
 	for _, file := range c.cfg.Files {
-		f, err := os.Open(file.Path)
-		if err != nil {
-			return nil, fmt.Errorf("os open %s: %w", file.Path, err)
-		}
-
 		outFileName, err := c.getFileName(file.OutputDir, file.Path)
 		if err != nil {
 			return nil, fmt.Errorf("get file name: %w", err)
@@ -64,9 +59,19 @@ func (c *Compressor) Compress() ([]byte, error) {
 			return nil, fmt.Errorf("zip file create %s: %w", file.OutputDir, err)
 		}
 
+		f, err := os.Open(file.Path)
+		if err != nil {
+			return nil, fmt.Errorf("os open: %w", err)
+		}
+
 		_, err = io.Copy(zf, f)
 		if err != nil {
 			return nil, fmt.Errorf("compress file %s: %w", file.Path, err)
+		}
+
+		err = f.Close()
+		if err != nil {
+			return nil, fmt.Errorf("file %s close: %w", file.Path, err)
 		}
 
 		c.logger.Info().String("zip_file", outFileName).Print("Compressed file")
