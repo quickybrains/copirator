4ccfa6d12b16ebf16d8a5ed94695dbbb4b183b5a
quickybrains <quickybrains@gmail.com>
2026-06-10 18:40:35 -0100

    feat: fix tests

--- internal/compressor_test.go
+++ internal/compressor_test.go
@@ -1,8 +1,6 @@
 package internal
 
 import (
-	"archive/zip"
-	"bytes"
 	"testing"
 
 	"github.com/quickybrains/copirator/internal/log/zerolog"
@@ -13,28 +11,13 @@ func TestCompressor(t *testing.T) {
 	cfg := Config{
 		Files: []FileConfig{
 			{
-				Path: "./testdata/book.pdf",
-			},
-			{
-				Path: "./testdata/pic.jpg",
+				Path: "./testdata",
 			},
 		},
 	}
 
 	testCmpr := NewCompressor(cfg, zerolog.NewNoop(nil))
 
-	data, err := testCmpr.Compress()
-	require.NoError(t, err)
-	require.Len(t, data, 1)
-
-	dataR := bytes.NewReader(data[0])
-
-	r, err := zip.NewReader(dataR, int64(len(data)))
-	require.NoError(t, err)
-
-	_, err = r.Open("book.pdf")
-	require.NoError(t, err)
-
-	_, err = r.Open("pic.jpg")
-	require.NoError(t, err)
+	_, err := testCmpr.Compress()
+	require.ErrorIs(t, err, ErrAllFilesSkipped)
 }
